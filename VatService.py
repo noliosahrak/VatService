65a36890d00608b8de968f0cc0bae853adacca1b
noliosahrak <wsburghardt@gmail.com>
2021-03-23 13:33:43 +0100

    add VatProvider class and mock it in tests

--- VatService.py
+++ VatService.py
@@ -1,4 +1,4 @@
-from VatProvider import VatProvider
+from numbers import Number
 
 
 class VatService:
@@ -9,13 +9,16 @@ class VatService:
     def get_gross_price_for_default_vat(self, product):
         return self.calculate_gross_price(product.get_net_price(), self.vat_provider.get_default_vat())
 
-    def get_gross_price(self, product):
-        vat_value = self.vat_provider.get_vat_for_type(product.get_product_type())
-        return self.calculate_gross_price(product.get_net_price(), vat_value)
+    def get_gross_price(self, net_price, product_type):
+        vat_value = self.vat_provider.get_vat_for_type(product_type)
+        return self.calculate_gross_price(net_price, vat_value)
 
     @staticmethod
     def calculate_gross_price(net_price, vat_value):
-        if vat_value > 1:
+        if not isinstance(vat_value, Number):
+            raise Exception("VAT should be numeric value")
+        elif vat_value > 1:
             raise Exception("VAT should be lower")
+        elif vat_value < 0:
+            raise Exception("VAT should have positive value")
         return round(net_price * (1 + vat_value), 2)
-
--- test_VatService.py
+++ test_VatService.py
@@ -9,7 +9,7 @@ from VatService import VatService
 
 
 class TestVatService:
-    @pytest.fixture()
+    @pytest.fixture
     def vat(self):
         self.vat_provider = mock.Mock()
         self.vat_service = VatService(self.vat_provider)
@@ -31,15 +31,25 @@ class TestVatService:
         self.vat_provider.get_vat_for_type.return_value = 0.05
 
         # when
-        result = self.vat_service.get_gross_price(product)
+        result = self.vat_service.get_gross_price(product.get_net_price(), product.get_product_type())
 
         # then
         assert_that(result).is_equal_to(4.2)
 
-    def test_should_raise_exception_when_vat_is_to_high(self, vat):
+    def test_should_raise_exception_when_vat_is_not_number(self, vat):
+        # then
+        assert_that(self.vat_service.calculate_gross_price).raises(Exception).when_called_with(1, "a4g5") \
+            .is_equal_to("VAT should be numeric value")
+
+    def test_should_raise_exception_when_vat_is_negative(self, vat):
         # then
         assert_that(self.vat_service.calculate_gross_price).raises(Exception).when_called_with(1, 10) \
             .is_equal_to("VAT should be lower")
+
+    def test_should_raise_exception_when_vat_is_to_high(self, vat):
+        # then
+        assert_that(self.vat_service.calculate_gross_price).raises(Exception).when_called_with(1, -1) \
+            .is_equal_to("VAT should have positive value")
 
     @staticmethod
     def generate_product(net_price, product_type):
